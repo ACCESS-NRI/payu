b0649515f0c9614b1eb59dcf85389a3ad4be799a
Jo Basevi <johanna.basevi@anu.edu.au>
2025-07-07 12:42:06 +1000

    Add exec_postfix field to add string after executable in the model run cmd
    - Defaults to None in Model class
    - Set to model_config file in ROMS model driver

--- payu/experiment.py
+++ payu/experiment.py
@@ -624,6 +624,9 @@ class Experiment(object):
             # older MPI libraries complained executable was not in PATH
             model_prog.append(os.path.join(model.work_path, model.exec_name))
 
+            if model.exec_postfix:
+                model_prog.append(model.exec_postfix)
+
             mpi_progs.append(' '.join(model_prog))
 
         cmd = '{runcmd} {flags} {exes}'.format(
